9f387d09d9e20811e343131a53ae53dee55c272a
Cameron Sun <cameron.csun@gmail.com>
2014-02-08 11:00:29 -0500

    Move to node.js

---
+++ server.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var journey = require('journey');
+
+var databaseUrl = 'plateonic';
+var collections = ["meals"];
+var db = require('mongojs');
+db.connect(databaseUrl, collections);
+
+
+var router = new(journey.Router);
+router.post('/new').bind(newMeal);
+
+function newMeal(request, response, data) {
+	var existingMeals = db.meals.find({user_id:data.user_id});
+	if(existingMeals) {
+		db.meals.update({user_id:data.user_id}, {$set:data}, checkDBError);
+	}
+}
+
+function checkDBError(err, updated) {
+	if(err || !updated) {
+		console.log('ERROR');
+	}
+}
+
+
+require('http').createServer(function (request, response) {
+	var body = "";
+
+	request.addListener('data', function (chunk) { body += chunk });
+	request.addListener('end', function () {
+        //
+        // Dispatch the request to the router
+        //
+        router.handle(request, body, function (result) {
+        	response.writeHead(result.status, result.headers);
+        	response.end(result.body);
+        });
+    });
+}).listen(8080);
