38b56c87f8879311efce4014a3c1303e4e09c7e1
Cameron Sun <cameron.csun@gmail.com>
2014-02-08 01:47:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plateonic::Application.config.secret_key_base = '89fec32574f9754fa6f01a65f6871ad53c7be8852bfd1cf0e0b8e11e8407af67eabd3b530717560fe830256e32c66925ba6591994a2347ba6f271820c67c0bf2'
