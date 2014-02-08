58932c4e30c0ea0cfad568c181e62816957b827f
singhbgithub <singh.b@husky.neu.edu>
2014-02-08 11:17:34 -0500

    Update server.js
    added error checking

--- server.js
+++ server.js
@@ -11,15 +11,21 @@ var router = new(journey.Router);
 router.post('/new').bind(newMeal);
 
 function newMeal(request, response, data) {
-	var existingMeals = db.meals.find({user_id:data.user_id});
-	if(existingMeals) {
+	var existingMeals = db.meals.find({user_id:data.user_id}, function (err, found) {
+		if (err || !found) {
+			console.log('ERROR existing meals find');
+		}
+	});
+
+	if (existingMeals) {
 		db.meals.update({user_id:data.user_id}, {$set:data}, checkDBError);
 	}
+
 }
 
 function checkDBError(err, updated) {
 	if(err || !updated) {
-		console.log('ERROR');
+		console.log('ERROR update');
 	}
 }
 
