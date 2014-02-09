d38d9023c0fa20a8d053ba18a4c1611cdb8eb11b
Cameron Sun <cameron.csun@gmail.com>
2014-02-08 19:48:33 -0500

    Cleaned up code. Now validates data.

--- server.js
+++ server.js
@@ -9,24 +9,86 @@ var db = mongojs(databaseUrl, collections);
 
 
 var router = new(journey.Router);
+
+
+/*
+* ====================================================
+* POST /new
+* Create a new meal for a user. Send back matched users
+*/
 router.post('/new').bind(newMeal);
 
-
-
-var MAX_MATCH_DISTANCE = 3
-
-
 function newMeal(request, response, data) {
-	sendMatchedUsers(response, data);
-	data.creation_time = new Date().getTime();
-	db.meals.update({ user_id: data.user_id }, data, { upsert: true });
+	if(!validateData(data)) {
+		sendFail(response);
+	}
+	else {
+		sendMatchedMeals(response, data);
+		data.creation_time = new Date().getTime();
+		db.meals.update({ user_id: data.user_id }, data, { upsert: true });
+	}
 }
 
-function sendMatchedUsers(response, data) {
-	var matched = db.meals.find({user_id: { $ne: data.user_id }, restaurant_id: data.restaurant_id});
-	matched.limit(10).toArray(function(err,arr) { response.send(200, {}, {matched:arr}) });
+function validateData(data) {
+	return ('user_id' in data) && ('restaurant_id' in data) && ('phone_number' in data);
 }
 
+/*
+* ====================================================
+* POST /update
+* Get all meals that match with a given user's current
+* meal
+*/
+router.post('/update').bind(getMatchedMeals)
+
+function getMatchedMeals(request, response, data) {
+	var mealData = db.meals.find({ user_id: data.user_id });
+
+	if(!mealData) {
+		sendFail(response);
+	}
+	else {
+		sendMatchedMeals(response, mealData);
+	}
+}
+
+function sendMatchedMeals(response, data) {
+	var matches = matchMeals(data);
+
+	matches.limit(10).toArray(function(err,arr) {
+		if(err) {
+			sendFail(response);
+		}
+		else {
+			sendSuccess(response, { matches: arr });
+		}
+	});
+}
+
+function matchMeals(data) {
+	return db.meals.find(
+	{
+		user_id: { $ne: data.user_id },
+		restaurant_id: data.restaurant_id,
+		creation_time: { $lt: new Date().getTime() }
+	});
+}
+
+/*
+* ====================================================
+*/
+function sendFail(response) {
+	response.send(200, {}, { status: "failed" });
+}
+
+function sendSuccess(response, data) {
+	data.status = "success";
+	response.send(200, {}, data);
+}
+
+/*
+* ====================================================
+*/
 require('http').createServer(function (request, response) {
 	var body = "";
 
@@ -44,4 +106,4 @@ require('http').createServer(function (request, response) {
 
 console.log('starting');
 
-// Use curl -X POST -H "Content-Type: application/json" -d '{"user_id":"ok","food_preference":"no","location":{"longitude":31,"latitude":23}}' http://localhost:8080/store
+// curl -X POST -H "Content-Type: application/json" -d '{"user_id":0,"restaurant_id": 1,"phone_number": 1}' http://localhost:8080/new
